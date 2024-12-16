56142eaa4b661c7c54f6a5cb2ac7bda2e391cac1
Einari Mäkiranta <makirantaeinari@gmail.com>
2024-12-16 21:38:34 +0200

    Removed Renderer print function definitions from engine.cpp

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,17 +1,4 @@
 #include "renderer.hpp"
-#include <iostream>
 
 namespace engine {
-
-	void Renderer::PrintMsg(const char* msg) {
-		std::cout << "renderer message: " << msg << '\n';
-	}
-
-	void Renderer::PrintWarn(const char* warn) {
-		std::cout << "renderer warning: " << warn << '\n';
-	}
-
-	void Renderer::PrintErr(const char* err, Renderer::ErrorOrigin origin) {
-		std::cout << "renderer error: " << err << " error origin: " << Renderer::ErrorOriginStr(origin) << '\n';
-	}
 }
