0c071e984b6150708c67afa6a61592673e7904be
Einari Mäkiranta <makirantaeinari@gmail.com>
2025-02-01 22:46:05 +0200

    Updated main.cpp

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,41 +5,11 @@
 class Player : public engine::World::Entity {
 public:
 
-	static inline Player* s_Instance = nullptr;
-	
 	engine::World& m_World;
 	engine::StaticMesh& m_Mesh;
 	engine::Quaternion m_Rotation;
 	engine::PersistentReference<engine::Body> m_Body;
-	engine::PersistentReference<engine::World::RenderData> m_RenderData;
-
-	/*
-	static engine::Vec3 MovementVectorUpdate(const engine::Body& creature) {
-		using namespace engine;
-		using Key = Input::Key;
-		return Vec3(
-				Input::ReadKeyValue(Key::D) - Input::ReadKeyValue(Key::A),
-				0.0f,
-				Input::ReadKeyValue(Key::W) - Input::ReadKeyValue(Key::S)) * (5 * Time::DeltaTime());
-	}
-
-	static void MoveCallback(const engine::Creature& creature, const engine::Vec3& position, const engine::Vec3& deltaPositio) {
-		using namespace engine;
-		s_Instance->m_RenderData.m_Val->m_Transform[3] = Vec4(position, 1.0f);
-	}
-
-	static void CameraFollowCallback(const engine::Creature& creature, engine::Vec3& outCameraPosition, engine::Vec3& outCameraLookAt,
-		uint32_t directionalLightCount, engine::UnidirectionalLight directionalLights[]) {
-		using namespace engine;
-		const engine::Vec3& creaturePos = creature.GetPosition();
-		static const Vec3 cameraOffset(20.0f, 20.0f, -20.0f);
-		static const Vec3 lookAtOffset(0.0f, 0.0f, 10.0f);
-		static const Vec3 lightOffset(50.0f, 50.0f, 2.0f);
-		outCameraPosition = creaturePos + cameraOffset;
-		outCameraLookAt = creaturePos + lookAtOffset;
-		directionalLights[0].SetViewMatrix(Mat4::LookAt(creaturePos + lightOffset, Vec3::Up(), creaturePos));
-	}
-	*/
+	engine::PersistentReference<engine::WorldRenderData> m_RenderData;
 
 	Player(engine::World& world, engine::StaticMesh& mesh) 
 		: m_World(world), 
@@ -50,19 +20,27 @@ public:
 					.u_TypeInfo { .m_PoleInfo { .m_Radius = 1, .m_Height = 2 } },
 				}
 			)), 
-			m_Mesh(mesh), m_RenderData(world.AddRenderData(*m_Body, {}, {})) {
+		m_Mesh(mesh), m_RenderData(world.AddRenderData(engine::WorldRenderDataFlag_NoSave, *m_Body, engine::Mat4(1), mesh.GetMeshData())) {
 		using namespace engine;
-		s_Instance = this;
-		World::RenderData& renderData = *m_RenderData;
-		renderData.m_Transform = engine::Mat4(1);
-		renderData.m_Transform[3].z = 3;
-		renderData.m_MeshData = m_Mesh.GetMeshData();
-		Creature& creature = *m_Creature;
-		creature.m_MovementVectorUpdate = MovementVectorUpdate;
-		creature.m_MoveCallback = MoveCallback;
-		creature.m_CameraFollowCallback = CameraFollowCallback;
-		world.SetCameraFollowCreature(*m_Creature);
-	}	
+	}
+
+	void Update(engine::World& world) {
+		using namespace engine;
+		using Key = Input::Key;
+		Vec3 movementVector = Vec3(
+			Input::ReadKeyValue(Key::D) - Input::ReadKeyValue(Key::A),
+			0.0f,
+			Input::ReadKeyValue(Key::W) - Input::ReadKeyValue(Key::S)
+		) * (5 * Time::DeltaTime());
+		Body& body = *m_Body;
+		if (movementVector != Vec3(0.0f)) {
+			body.Move(body.GetPosition() + movementVector);
+			world.SetGameCameraView(Mat4::LookAt(Vec3(0.0f, 10.0f, -5.0f), Vec3::Up(), body.GetPosition()));
+		}
+	}
+
+	void Terminate() {
+	}
 };
 
 class UIElement : public engine::UI::Entity {
@@ -177,7 +155,7 @@ int main() {
 
 	GLFWwindow* pWindow = glfwCreateWindow(540, 540, "Test", nullptr, nullptr);
 
-	Engine engine(engine::EngineMode_Play, "Test", pWindow, 1000);	
+	Engine engine(engine::EngineMode_Play, "Test", pWindow, 1000);
 	Renderer& renderer = engine.GetRenderer();
 	TextRenderer textRenderer = engine.GetTextRenderer();
 
@@ -198,49 +176,58 @@ int main() {
 	Array<Vertex, 4> quadVertices;
 	Array<uint32_t, 6> quadIndices;
 
-	Engine::GetQuadMesh(quadVertices, quadIndices);
-
-	LogicMesh logicQuadMesh(quadVertices, quadIndices);
-
-	Mat4 groundTransform = Quaternion::AxisRotation(Vec3(1.0f, 0.0f, 0.0f), -pi / 4).AsMat4();
-	groundTransform[0] *= 100.0f;
-	groundTransform[1] *= 100.0f;
-	groundTransform[2] *= 100.0f;
-	groundTransform[3] = { 0.0f, -1.0f, 0.0f, 1.0f };
-	
-	World::Ground::CreateInfo groundInfo {
-		.m_LogicMesh = logicQuadMesh,
-		.m_Transform = groundTransform,
-	};
-
-	World::Obstacle::CreateInfo obstacleInfo {
-		.m_Position = {},
-		.m_YRotation = pi / 4,
-		.m_ColliderInfo {
-			.m_LocalPosition = {},
-			.m_Type = Collider::Type::Fence,
-			.u_TypeInfo {
-				.m_FenceInfo { .m_Dimensions { 4, 4, 4 }, .m_YRotation = 0 },
-			}
-		},
-	};
-
-	World& world = engine.LoadWorld({ 128, 128 }, { 8, 8 }, 1, &groundInfo, 1, &obstacleInfo);
-
 	uint8_t* brickWallImage;
 	Vec2_T<uint32_t> brickWallExtent;
 	assert(LoadImage("resources\\textures\\brick_wall\\albedo.png", 4, brickWallImage, brickWallExtent));
 	StaticTexture brickWallTexture(renderer);
 	assert(brickWallTexture.Create(VK_FORMAT_R8G8B8A8_SRGB, brickWallExtent, brickWallImage));
-	FreeImage(brickWallImage);
+	engine::FreeImage(brickWallImage);
+
+	World& world = engine.GetWorld();
+
 	World::TextureMap textureMap{};
 	assert(world.CreateTextureMap(brickWallTexture, textureMap));
 
-	const DynamicArray<World::Ground>& grounds = world.GetGrounds();
+	Area& area = world.AddArea(AreaFlag_NoSave);
+
+	Engine::GetQuadMesh(quadVertices, quadIndices);
+
+	LogicMesh logicQuadMesh(quadVertices, quadIndices);
+
+	auto obstacle = area.AddObstacle(
+		{
+			.m_Position { 0, 0, 0 },
+			.m_YRotation = pi / 2,
+			.m_ColliderInfo {
+				.m_LocalPosition = {},
+				.m_Type = Collider::Type::Fence,
+				.u_TypeInfo {
+					.m_FenceInfo {
+						.m_Dimensions { 2, 2, 2 },
+						.m_YRotation = 0,
+					}
+				}
+			}
+		}
+	);
+
+	Mat4 groundTransform = Quaternion::AxisRotation(Vec3(1.0f, 0.0f, 0.0f), -pi / 2).AsMat4();
+
+	groundTransform[0] *= 10;
+	groundTransform[1] *= 10;
+	groundTransform[2] *= 10;
+	groundTransform[3].y = -1.0f;
+
+	auto ground = area.AddRayTarget(
+		{
+			.m_LogicMesh = logicQuadMesh,
+			.m_Transform = groundTransform,
+		}
+	);
 
 	MeshData groundMesh = engine.GetQuadMesh().GetMeshData();
 
-	auto groundRenderData = world.AddRenderData(grounds[0], groundTransform, groundMesh);
+	auto groundRenderData = world.AddRenderData(WorldRenderDataFlag_NoSave, *ground, groundTransform, engine.GetQuadMesh().GetMeshData());
 	(*groundRenderData).m_AlbedoTextureDescriptorSet = textureMap.m_DescriptorSet;
 
 	Obj cubeObj{};
@@ -257,22 +244,14 @@ int main() {
 	StaticMesh cubeMesh(renderer);
 	cubeMesh.CreateBuffers(cubeVertices.Size(), cubeVertices.Data(), cubeIndices.Size(), cubeIndices.Data());
 
-	const DynamicArray<World::Obstacle>& obstacles = world.GetObstacles();
-
-	Mat4 obstacleTransform = Quaternion::AxisRotation(Vec3::Up(), obstacleInfo.m_YRotation).AsMat4();
-	for (size_t i = 0; i < 3; i++) {
-		obstacleTransform[i] *= 2;
-	}
-
-	auto obstacleRenderData = world.AddRenderData(obstacles[0], obstacleTransform, cubeMesh.GetMeshData());
+	auto obstacleRenderData = world.AddRenderData(WorldRenderDataFlag_NoSave, *obstacle, Mat4(1), cubeMesh.GetMeshData());
 	(*obstacleRenderData).m_AlbedoTextureDescriptorSet = textureMap.m_DescriptorSet;
-	//world.AddDebugRenderData(obstacles[0], obstacleTransform, Vec4(0.0f, 0.8f, 0.3f, 1.0f), cubeMesh.GetMeshData());
-
-	auto obstacleRenderData2 = world.AddRenderData(obstacles[0], groundTransform, engine.GetQuadMesh().GetMeshData());
-	(*obstacleRenderData2).m_AlbedoTextureDescriptorSet = textureMap.m_DescriptorSet;
 
 	Player player(world, cubeMesh);
 	(*player.m_RenderData).m_AlbedoTextureDescriptorSet = textureMap.m_DescriptorSet;
+
+	world.AddEntity(&player);
+
 	//NPC npc(world, playerMesh);
 
 	while (engine.Loop()) {
