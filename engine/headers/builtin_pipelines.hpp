e434da4a9273bae181c7799e465917195a3fb09e
Einari Mäkiranta <makirantaeinari@gmail.com>
2025-02-08 21:08:32 +0200

    Added Editor pipelines class

--- engine/headers/builtin_pipelines.hpp
+++ engine/headers/builtin_pipelines.hpp
@@ -9,7 +9,7 @@ namespace engine {
 namespace pipelines {
 
 	class World {
-		public:
+	public:
 
 		VkPipeline m_DrawPipelinePBR = VK_NULL_HANDLE;
 		VkPipelineLayout m_DrawPipelineLayoutPBR = VK_NULL_HANDLE;
@@ -31,18 +31,50 @@ namespace pipelines {
 		void Initialize(engine::Renderer& renderer, VkFormat colorImageResourceFormat);
 
 		void Terminate(engine::Renderer& renderer) {
-			renderer.DestroyDescriptorSetLayout(m_CameraDescriptorSetLayout);
-			renderer.DestroyDescriptorSetLayout(m_RenderPBRImagesDescriptorSetLayout);
-			renderer.DestroyDescriptorSetLayout(m_DirectionalLightShadowMapDescriptorSetLayout);
-			renderer.DestroyDescriptorSetLayout(m_TextureDescriptorSetLayoutPBR);
 			renderer.DestroyPipeline(m_DrawPipelinePBR);
+			m_DrawPipelinePBR = VK_NULL_HANDLE;
 			renderer.DestroyPipelineLayout(m_DrawPipelineLayoutPBR);
+			m_DrawPipelineLayoutPBR = VK_NULL_HANDLE;
 			renderer.DestroyPipeline(m_DrawPipelineUD);
+			m_DrawPipelineUD = VK_NULL_HANDLE;
 			renderer.DestroyPipelineLayout(m_DrawPipelineLayoutUD);
+			m_DrawPipelineLayoutUD = VK_NULL_HANDLE;
 			renderer.DestroyPipeline(m_RenderPipelinePBR);
+			m_RenderPipelinePBR = VK_NULL_HANDLE;
 			renderer.DestroyPipelineLayout(m_RenderPipelineLayoutPBR);
+			m_RenderPipelineLayoutPBR = VK_NULL_HANDLE;
 			renderer.DestroyPipeline(m_WirePipeline);
+			m_WirePipeline = VK_NULL_HANDLE;
 			renderer.DestroyPipelineLayout(m_DebugPipelineLayout);
+			m_DebugPipelineLayout = VK_NULL_HANDLE;
+			renderer.DestroyDescriptorSetLayout(m_CameraDescriptorSetLayout);
+			m_CameraDescriptorSetLayout = VK_NULL_HANDLE;
+			renderer.DestroyDescriptorSetLayout(m_RenderPBRImagesDescriptorSetLayout);
+			m_RenderPBRImagesDescriptorSetLayout = VK_NULL_HANDLE;
+			renderer.DestroyDescriptorSetLayout(m_DirectionalLightShadowMapDescriptorSetLayout);
+			m_DirectionalLightShadowMapDescriptorSetLayout = VK_NULL_HANDLE;
+			renderer.DestroyDescriptorSetLayout(m_TextureDescriptorSetLayoutPBR);
+			m_TextureDescriptorSetLayoutPBR = VK_NULL_HANDLE;
 		}
-	};	
+	};
+
+	class Editor {
+	public:
+		
+		VkPipeline m_TorusPipeline = VK_NULL_HANDLE;
+		VkPipelineLayout m_TorusPipelineLayout = VK_NULL_HANDLE;
+
+		VkDescriptorSetLayout m_DebugRenderTransformDescriptorSetLayout = VK_NULL_HANDLE;
+
+		void Initialize(engine::Renderer& renderer);
+
+		void Terminate(engine::Renderer& renderer) {
+			renderer.DestroyPipeline(m_TorusPipeline);
+			m_TorusPipeline = VK_NULL_HANDLE;
+			renderer.DestroyPipelineLayout(m_TorusPipelineLayout);
+			m_TorusPipelineLayout = VK_NULL_HANDLE;
+			renderer.DestroyDescriptorSetLayout(m_DebugRenderTransformDescriptorSetLayout);
+			m_DebugRenderTransformDescriptorSetLayout = VK_NULL_HANDLE;
+		}
+	};
 }
